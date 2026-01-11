3e8b6e9e8320f8dcf1aab1c7d5c709562a02a06d
YuLetian <yu.letian66@gmail.com>
2026-01-12 00:02:34 +0800

    1.2

---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,22 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by SKY on 11/1/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
