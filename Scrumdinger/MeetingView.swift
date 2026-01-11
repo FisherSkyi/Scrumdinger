8a3f3b324e8f3bd6af4f9a1c570eccff25f7dccc
YuLetian <yu.letian66@gmail.com>
2026-01-11 23:16:10 +0800

    1.1

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by SKY on 11/1/26.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Second Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Second Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
