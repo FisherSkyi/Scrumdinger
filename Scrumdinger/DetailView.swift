765d04c6b86cb34918bb2f73b47b174c45b88f58
YuLetian <yu.letian66@gmail.com>
2026-01-12 11:01:24 +0800

    3.1

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,54 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by SKY on 12/1/26.
+//
+
+import SwiftUI
+import ThemeKit
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(header: Text("MEETING INFO")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) min")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(6)
+                }
+                .accessibilityElement(children: .combine)
+            }
+            
+            Section(header: Text("ATTENDEES")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
+            }
+        }
+        .navigationTitle(scrum.title)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
